61a1e29ed9e3b34859cd86d54617347a77e5e675
Amy <leiamy12@gmail.com>
2020-01-26 23:06:27 -0500

    added styling to indicate which widget is being viewed

---
+++ client/src/components/modules/WidgetButton.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class WidgetButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: null,
+    };
+  }
+
+  handleOnClick = (name) => {
+    this.props.handleOnClick(name);
+    this.setState({
+      active: name,
+    });
+  };
+
+  render() {
+    const buttons = this.props.widgetlist.map((widget, k) => (
+      <button
+        key={k}
+        className={`calendar-widget-btn ${this.state.active == widget.name ? "active" : ""}`}
+        type="button"
+        onClick={() => this.handleOnClick(widget.name)}
+      >
+        {widget.name}
+      </button>
+    ));
+    return <> {buttons} </>;
+  }
+}
+
+export default WidgetButton;
