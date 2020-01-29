ac4c8a53e9644f09e4ce81280c7506e94e79dd8f
Amy <leiamy12@gmail.com>
2020-01-29 00:48:25 -0500

    styled collections into a box and made btn to create new stand out

--- client/src/components/modules/Popup.js
+++ client/src/components/modules/Popup.js
@@ -22,48 +22,51 @@ class Popup extends Component {
 
   render() {
     return (
-      <div className="popup">
-        <div className="popup\_inner">
-          <p>{this.props.text}</p>
-          {this.props.submitType === "input" && (
-            <>
-              <input
-                type="text"
-                className="popup-text"
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
+      <>
+        <div className="popup">
+          <div className="popup\_inner">
+            <p>{this.props.text}</p>
+            {this.props.submitType === "input" && (
+              <>
+                <input
+                  type="text"
+                  className="popup-text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+                <div className="popup-btn-container">
+                  <button
+                    className="popup-btn popup-submit-btn"
+                    onClick={() => this.props.editFunction(this.state.value)}
+                  >
+                    Submit
+                  </button>
+                  <button className="popup-btn" onClick={this.props.closePopup}>
+                    Cancel
+                  </button>
+                </div>
+                {this.props.nameError === "Duplicate name" && (
+                  <div className="popup-error">You already have a collection with this name!</div>
+                )}
+                {this.props.nameError === "No name entered" && (
+                  <div className="popup-error">Collection name cannot be blank.</div>
+                )}
+              </>
+            )}
+            {this.props.submitType === "binary" && (
               <div className="popup-btn-container">
-                <button
-                  className="popup-btn popup-submit-btn"
-                  onClick={() => this.props.editFunction(this.state.value)}
-                >
-                  Submit
+                <button className="popup-btn popup-delete-btn" onClick={this.props.closePopup}>
+                  No
                 </button>
-                <button className="popup-btn" onClick={this.props.closePopup}>
-                  Cancel
+                <button className="popup-btn" onClick={() => this.props.editFunction()}>
+                  Yes, delete
                 </button>
               </div>
-              {this.props.nameError === "Duplicate name" && (
-                <div className="popup-error">You already have a collection with this name!</div>
-              )}
-              {this.props.nameError === "No name entered" && (
-                <div className="popup-error">Collection name cannot be blank.</div>
-              )}
-            </>
-          )}
-          {this.props.submitType === "binary" && (
-            <div className="popup-btn-container">
-              <button className="popup-btn popup-delete-btn" onClick={this.props.closePopup}>
-                No
-              </button>
-              <button className="popup-btn" onClick={() => this.props.editFunction()}>
-                Yes, delete
-              </button>
-            </div>
-          )}
+            )}
+          </div>
         </div>
-      </div>
+        <div className="overlay" onClick={this.props.closePopup}></div>
+      </>
     );
   }
 }
